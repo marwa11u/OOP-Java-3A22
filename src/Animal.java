17433bf881081626556ce044c35df304adf13f18
marwau11 <marwa.mokhtar@esprit.tn>
2024-09-22 09:39:47 +0100

    prosit 2

---
+++ src/Animal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Animal {
+    String family;
+    String name;
+    int age;
+    boolean isMammal;
+
+    public Animal(String famille, String nom, int ag, boolean is){
+        family=famille;
+        name=nom;
+        age=ag;
+        isMammal=is;
+    }
+    public void displayAnimal(){
+        System.out.println("family: "+family+" name: " + name + " age: "+age+" isMammal: "+isMammal);
+
+    }
+
+    public String toString() {
+        return ("family: "+family+" name: " + name + " age: "+age+" isMammal: "+isMammal);
+    }
+
+}
